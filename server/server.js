2ce83bae9ef474e52c7ec5b8986ea28955b590cd
fokafor86 <felixokafor46@yahoo.com>
2019-09-09 14:43:24 +0200

    babel added

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import routes from './routes/routes';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port,() => {
+    console.log(`server started at ${port}`)
+})
+
+export default app;
+
